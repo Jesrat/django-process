6a83e888e886a4295b870d8aa2a03be7f7573819
Josue Gomez <jgomez@binkfe.com>
2019-09-16 09:36:58 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-process",
+    version="1.0",
+    author="Josue Gomez",
+    author_email="jgomez@binkfe.com",
+    description="A package for create process and tasks on django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.binkfe.com/jesrat/django-process",
+    packages=['process'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Framework :: Django',
+        "Operating System :: OS Independent",
+    ],
+)
