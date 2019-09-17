2dbb5386a4d3796005e18535f3fcfcd931b22784
Josue Gomez <jgomez@binkfe.com>
2019-09-17 11:04:59 -0600

    release version 3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-process",
-    version="2.2",
+    version="3.0",
     author="Josue Gomez",
     author_email="jgomez@binkfe.com",
     description="A package for create process and tasks on django",
